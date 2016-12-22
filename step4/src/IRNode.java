79e35b91605efc5c999ba3d2dff0a84f555f628a
Xiao Xiao <xiao81@purdue.edu>
2016-12-21 21:53:23 -0500

    Finished IRNode.java

--- step4/src/IRNode.java
+++ step4/src/IRNode.java
@@ -9,6 +9,29 @@ public class IRNode {
 		this.result = inputResult;
 	}
 
-	public
+	public String getOpCode() {
+		return this.opCode;
+	}
+
+	public String getOperand1() {
+		return this.operand1;
+	}
+
+	public String getOperand2() {
+		return this.operand2;
+	}
+
+	public String getResult() {
+		return this.result;
+	}
+
+	public void printNode() {
+		String operand1 = (this.operand1 == null) ? "" : " " + this.operand1;
+		String operand2 = (this.operand2 == null) ? "" : " " + this.operand2;
+		String opCode = (this.opCode == null) ? "" : " " + this.opCode;
+		String result = (this.result == null) ? "" : " " + this.result;
+
+		System.out.println(";" + opCode + operand1 + operand2 + result);
+	}
 
 }
