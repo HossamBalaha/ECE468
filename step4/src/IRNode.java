f35841bda7f771d15e060fddba348c4ac9aa5300
Xiao Xiao <xiao81@purdue.edu>
2016-12-21 21:37:01 -0500

    Initialze IRNode.java

---
+++ step4/src/IRNode.java
@@ -0,0 +1,14 @@
+public class IRNode {
+	
+	private String opCode, operand1, operand2, result;
+
+	public IRNode(String inputOpCode, String inputOperand1, String inputOperand2, String inputResult) {
+		this.opCode = inputOpCode;
+		this.operand1 = inputOperand1;
+		this.operand2 = inputOperand2;
+		this.result = inputResult;
+	}
+
+	public
+
+}
